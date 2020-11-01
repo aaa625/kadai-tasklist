16362af8c4f55fbd17a6666449060cf63cdd307e
aaa.aaaa <gakusyuuyou.1013@gmail.com>
2020-11-01 16:25:00 +0000

    post

--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,5 @@ Rails.application.routes.draw do
   get 'signup', to: 'users#new'
   resources :tasks
   resources :users, only: [:index, :new, :create]
+  resources :microposts, only: [:create, :destroy]
 end
