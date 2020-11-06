334eda5507c3c3067be937795179dbcd4c45783e
aaa.aaaa <gakusyuuyou.1013@gmail.com>
2020-11-06 17:43:19 +0000

    userclass

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,9 +2,7 @@ class TasksController < ApplicationController
   before_action :require_user_logged_in
   before_action :set_task, only: [:show, :edit, :update, :destroy]
   def index
-    
-      @tasks = current_user.tasks.page(params[:page]).per(3)
-  
+    @tasks = current_user.tasks.page(params[:page]).per(3)
   end
 
   def show
