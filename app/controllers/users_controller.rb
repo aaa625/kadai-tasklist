c2dc1c76de9319a123e6f33734f6ce38e0b23bd2
aaa.aaaa <gakusyuuyou.1013@gmail.com>
2020-11-08 12:44:25 +0000

    userindex

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,13 @@
 class UsersController < ApplicationController
   
   
-  def index
-  end
-
   def new
     @user = User.new
   end
 
   def create
     @user = User.new(user_params)
+    
 
     if @user.save
       flash[:success] = 'ユーザを登録しました。'
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,6 @@ Rails.application.routes.draw do
 
   get 'signup', to: 'users#new'
  
-  resources :users, only: [:index, :new, :create]
+  resources :users, only: [:new, :create]
    resources :tasks
 end
